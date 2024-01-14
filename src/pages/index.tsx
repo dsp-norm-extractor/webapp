a108569dc3948bbe6d0928e4e8d6d21da3061326
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-01-14 23:10:34 +0100

    Use the extrinsic definition of games for index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,18 @@
+// pages/index.tsx
+
 import React, { useState } from "react"
 import FlexBox from "@/common/generic/flexbox"
-import { GameCard } from "@/components/game-card/game-card"
 import SearchBar from "@/common/generic/search-bar"
+import { Chip, Divider } from "@mui/material"
+import { useRouter } from "next/router"
+import Link from "next/link"
+import { initialGames } from "./games/games-list"
+import { titleToSlug } from "@/helpers/slug"
 
-const initialGames = [
-  {
-    title: "Catan",
-    image: "https://picsum.photos/seed/Catan/200/300",
-  },
-  {
-    title: "Ticket to Ride",
-    image: "https://picsum.photos/seed/Ticket-to-Ride/200/300",
-  },
-  {
-    title: "Carcassonne",
-    image: "https://picsum.photos/seed/Carcassonne/200/300",
-  },
-  {
-    title: "Pandemic",
-    image: "https://picsum.photos/seed/Pandemic/200/300",
-  },
-  {
-    title: "Codenames",
-    image: "https://picsum.photos/seed/Codenames/200/300",
-  },
-  {
-    title: "Splendor",
-    image: "https://picsum.photos/seed/Splendor/200/300",
-  },
-  {
-    title: "7 Wonders",
-    image: "https://picsum.photos/seed/7-Wonders/200/300",
-  },
-  {
-    title: "Dominion",
-    image: "https://picsum.photos/seed/Dominion/200/300",
-  },
-  {
-    title: "Risk",
-    image: "https://picsum.photos/seed/Risk/200/300",
-  },
-  {
-    title: "Monopoly",
-    image: "https://picsum.photos/seed/Monopoly/200/300",
-  },
-]
-
-export default function Home() {
+const Home: React.FC = () => {
   const [games, setGames] = useState(initialGames)
   const [searchTerm, setSearchTerm] = useState("")
+  const router = useRouter()
 
   const handleSearch = (term: string) => {
     setSearchTerm(term)
@@ -61,15 +25,36 @@ export default function Home() {
   return (
     <div>
       <SearchBar onSearch={handleSearch} />
-      <FlexBox>
+      <Divider
+        sx={{
+          mt: 5,
+          mb: 5,
+        }}>
+        <Chip
+          label="List of Games"
+          size="small"
+        />
+      </Divider>
+      <FlexBox
+        gap={2}
+        wrap="wrap"
+        customStyles={{ flexBasis: "calc(33.333% - 2px)" }}>
         {games.map(({ title, image }) => (
-          <GameCard
+          <Link
             key={title}
-            title={title}
-            image={image}
-          />
+            href={`/games/${titleToSlug(title)}`}>
+            <div>
+              <img
+                src={image}
+                alt={title}
+              />
+              <div>{title}</div>
+            </div>
+          </Link>
         ))}
       </FlexBox>
     </div>
   )
 }
+
+export default Home
