485bdb0f10a19710eb45b702147da70877ea7b61
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-01-15 19:29:32 +0100

    Connection with API

--- src/pages/api/handle-list.ts
+++ src/pages/api/handle-list.ts
@@ -1,15 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
     try {
       const { rules } = req.body
+      const rulesList: string[] = []
 
-      // Process the rules here, for example, log them
-      console.log("Received rules:", rules)
+      rules.map(({ component }: { component: string }) =>
+        rulesList.push(component)
+      )
 
-      // Respond with a success message
-      res.status(200).json({ success: true, rules: rules })
+      // Send a POST request to the backend
+      const backendUrl = "http://localhost:8000/predict_frame"
+      const backendResponse = await fetch(backendUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(rulesList),
+      })
+
+      if (backendResponse.ok) {
+        // Backend successfully processed the rules
+        const backendData = await backendResponse.json()
+        res.status(200).json({ success: true, backendData })
+      } else {
+        // Backend returned an error
+        console.error("Backend error:", backendResponse.statusText)
+        res.status(500).json({ success: false, error: "Backend Error" })
+      }
     } catch (error) {
       console.error("Error processing rules:", error)
       res.status(500).json({ success: false, error: "Internal Server Error" })
