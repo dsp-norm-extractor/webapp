29c25c49120fd1b22898ed808e614eb2e2cc7f1b
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-01-17 23:41:14 +0100

    Added react-hot-toaster for user feedback
    Big changes on the frame-viewer page
    - functionality to edit frames categories, add and remove frames

--- src/components/rule-details/rule-details.tsx
+++ src/components/rule-details/rule-details.tsx
@@ -3,7 +3,6 @@ import {
   Table,
   TableBody,
   TableCell,
-  TableHead,
   TableRow,
   Paper,
   Grid,
@@ -12,16 +11,89 @@ import {
   Chip,
   Divider,
   Button,
-  IconButton,
+  MenuItem,
+  ClickAwayListener,
+  Grow,
+  MenuList,
+  Popper,
+  InputAdornment,
+  FormControl,
+  Input,
+  InputLabel,
+  FormHelperText,
 } from "@mui/material"
-import React from "react"
+import React, {
+  FC,
+  KeyboardEvent,
+  SyntheticEvent,
+  useRef,
+  useState,
+} from "react"
 import Title from "../data/title"
 
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline"
 import DeleteIcon from "@mui/icons-material/Delete"
+import TaskIcon from "@mui/icons-material/Task"
 import { FlexBox } from "@/common/generic/flexbox.styled"
 
-const RuleDetails: React.FC<RuleDetailsProps> = ({ title, frames }) => {
+const RuleDetails: FC<
+  RuleDetailsProps & { onDelete: (sentnce: string, index: number) => void } & {
+    onFrameAdd: (sentence: string, frameType: string) => void
+  } & {
+    onFrameEdit: (
+      sentence: string,
+      frame: number,
+      field: string,
+      fipayload: any
+    ) => void
+  }
+> = ({ title, frames, onDelete, onFrameAdd, onFrameEdit }) => {
+  const [open, setOpen] = useState(false)
+
+  const [inputValues, setInputValues] = useState<any>(
+    Object.entries(frames).map(() => ({
+      acts: {},
+      facts: {},
+      duties: {},
+    }))
+  )
+
+  const anchorRef = useRef<HTMLButtonElement>(null)
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen)
+  }
+
+  const handleClose = (event: Event | SyntheticEvent) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return
+    }
+
+    setOpen(false)
+  }
+
+  const handleListKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "Tab") {
+      event.preventDefault()
+      setOpen(false)
+    } else if (event.key === "Escape") {
+      setOpen(false)
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open)
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current!.focus()
+    }
+
+    prevOpen.current = open
+  }, [open])
+
   return (
     <>
       <Grid
@@ -33,11 +105,11 @@ const RuleDetails: React.FC<RuleDetailsProps> = ({ title, frames }) => {
           {title}
         </Typography>
         <Divider
-          textAlign="left"
+          textAlign="right"
           sx={{
             mt: 2,
           }}>
-          <Chip label="Frames for selected sentence" />
+          <Chip label="Frames" />
         </Divider>
       </Grid>
       {Object.entries(frames).map(
@@ -63,22 +135,81 @@ const RuleDetails: React.FC<RuleDetailsProps> = ({ title, frames }) => {
                           category.charAt(0).toUpperCase() + category.slice(1)
                         }`}
                       </Title>
-                      <IconButton
+                      <Button
+                        component="label"
                         color="error"
-                        aria-label="delete-frame">
-                        <DeleteIcon />
-                      </IconButton>
+                        variant="outlined"
+                        startIcon={<DeleteIcon />}
+                        onClick={() => onDelete(title, index)}>
+                        Delete
+                      </Button>
                     </FlexBox>
 
                     <Table>
                       <TableBody>
-                        {data.map((act: any, index: any) => (
-                          <React.Fragment key={index}>
-                            {Object.entries(act).map(([key, value]) => (
+                        {data.map((frame: any, frameIndex: any) => (
+                          <React.Fragment key={frameIndex}>
+                            {Object.entries(frame).map(([key, value]) => (
                               <TableRow key={key}>
                                 <TableCell>{key}</TableCell>
-                                <TableCell style={{ textAlign: "left" }}>
-                                  {JSON.stringify(value)}
+                                <TableCell>
+                                  <FormControl
+                                    fullWidth
+                                    sx={{ m: 1 }}
+                                    variant="standard">
+                                    <InputLabel
+                                      htmlFor={`key-value-edit-${frameIndex}-${key}`}>
+                                      {key}
+                                    </InputLabel>
+                                    <Input
+                                      id={`key-value-edit-${frameIndex}-${key}`}
+                                      value={
+                                        inputValues[frameIndex][category][
+                                          key
+                                        ] || ""
+                                      }
+                                      endAdornment={
+                                        <InputAdornment position="end">
+                                          <Button
+                                            id="composition-button"
+                                            variant="text"
+                                            color="info"
+                                            endIcon={<TaskIcon />}
+                                            size="small"
+                                            onClick={() =>
+                                              onFrameEdit(
+                                                title,
+                                                frameIndex,
+                                                key,
+                                                inputValues[frameIndex][
+                                                  category
+                                                ][key]
+                                              )
+                                            }>
+                                            validate
+                                          </Button>
+                                        </InputAdornment>
+                                      }
+                                      onChange={(e) =>
+                                        setInputValues((inputValues: any) => {
+                                          const newInputValues = [
+                                            ...inputValues,
+                                          ]
+                                          newInputValues[frameIndex][category] =
+                                            {
+                                              ...newInputValues[frameIndex][
+                                                category
+                                              ],
+                                              [key]: e.target.value,
+                                            }
+                                          return newInputValues
+                                        })
+                                      }
+                                    />
+                                    <FormHelperText id="standard-weight-helper-text">{`${key}: ${JSON.stringify(
+                                      value
+                                    )}`}</FormHelperText>
+                                  </FormControl>
                                 </TableCell>
                               </TableRow>
                             ))}
@@ -95,13 +226,57 @@ const RuleDetails: React.FC<RuleDetailsProps> = ({ title, frames }) => {
       )}
       <Grid
         item
-        xs={4}>
-        <Button
-          component="label"
-          variant="outlined"
-          startIcon={<AddCircleOutlineIcon />}>
-          Add Frame
-        </Button>
+        xs={12}
+        justifyContent="center">
+        <FlexBox justifyContent="center">
+          <Button
+            ref={anchorRef}
+            id="composition-button"
+            aria-controls={open ? "composition-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+            variant="outlined"
+            startIcon={<AddCircleOutlineIcon />}>
+            Add Frame
+          </Button>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            placement="bottom-start"
+            transition
+            disablePortal>
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === "bottom-start" ? "left top" : "left bottom",
+                }}>
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="composition-menu"
+                      aria-labelledby="composition-button"
+                      onKeyDown={handleListKeyDown}>
+                      <MenuItem onClick={() => onFrameAdd(title, "act")}>
+                        Act
+                      </MenuItem>
+                      <MenuItem onClick={() => onFrameAdd(title, "fact")}>
+                        Fact
+                      </MenuItem>
+                      <MenuItem onClick={() => onFrameAdd(title, "duty")}>
+                        Duty
+                      </MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </FlexBox>
       </Grid>
     </>
   )
