4ac1598f06e96f1a46ac6182fb261a70e9bc75c9
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-01-21 22:10:50 +0100

    Display games on main page, based on what is in the localStorage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,35 @@
-import { FC, useState } from 'react'
-
+import { FC, useState, useEffect } from 'react'
 import { Chip, Container, Divider, Grid } from '@mui/material'
 import Link from 'next/link'
 
 import SearchBar from '@/components/common/generic/search-bar'
 import { GameCard } from '@/components/game-card/game-card'
 import { titleToSlug } from '@/helpers/slug'
+import { initialGames } from '@/helpers/games-list'
 
-import { initialGames } from '../helpers/games-list'
+interface Game {
+  title: string
+  image?: string
+}
 
 const Home: FC = () => {
-  const [games, setGames] = useState(initialGames)
+  const [games, setGames] = useState<Game[]>([])
   const [, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    const storedGames = localStorage.getItem('gameDetails')
+    if (storedGames) {
+      const gameDetails = JSON.parse(storedGames)
+      setGames([{ title: gameDetails.game, image: `https://picsum.photos/seed/${gameDetails.game}/300/200` }])
+    }
+  }, [])
 
   const handleSearch = (term: string) => {
     setSearchTerm(term)
-    const filteredGames = initialGames.filter((game) => game.title.toLowerCase().includes(term.toLowerCase()))
-    setGames(filteredGames)
+    if (games.length > 0) {
+      const filteredGames = games.filter(({ title }) => title.toLowerCase().includes(term.toLowerCase()))
+      setGames(filteredGames)
+    }
   }
 
   return (
@@ -33,7 +46,7 @@ const Home: FC = () => {
         {games.map(({ title, image }) => (
           <Grid item key={title} xs={12} sm={6} md={4}>
             <Link key={title} href={`/games/${titleToSlug(title)}`}>
-              <GameCard image={image} title={title} />
+              <GameCard image={image ?? 'asd'} title={title} />
             </Link>
           </Grid>
         ))}
